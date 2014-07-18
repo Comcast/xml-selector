cd7f6d5ffae559db96a93d6d9738af000e002b61
Jeff Hunter <jeff_hunter@sv.comcast.com>
2014-07-18 07:35:54 -0700

    Test constructor calls without new

--- test/xq_constructor.js
+++ test/xq_constructor.js
@@ -18,10 +18,35 @@ module.exports.testEmpty = function(test) {
 }
 
 /**
+ * Test the empty constructor without new
+ */
+module.exports.testEmptySansNew = function(test) {
+  var empty = xQ();
+  
+  test.ok(empty);
+  
+  test.strictEqual(empty.length, 0);
+  
+  test.done();
+}
+
+/**
  * Test the string constructor
  */
 module.exports.testString = function(test) {
   var q = new xQ("<doc><hello /></doc>");
+  
+  test.ok(q);
+  test.strictEqual(q.length, 1);
+  
+  test.done();
+}
+
+/**
+ * Test the string constructor without new
+ */
+module.exports.testStringSansNew = function(test) {
+  var q = xQ("<doc><hello /></doc>");
   
   test.ok(q);
   test.strictEqual(q.length, 1);
@@ -39,6 +64,21 @@ module.exports.testInvalid = function(test) {
   
   test.throws(function() {
     new xQ(5);
+  });
+  
+  test.done();
+}
+
+/**
+ * Test invalid constructor calls without new
+ */
+module.exports.testInvalidSansNew = function(test) {
+  test.throws(function() {
+    xQ("something's not right");
+  });
+  
+  test.throws(function() {
+    xQ(5);
   });
   
   test.done();
