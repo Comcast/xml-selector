6f986006a426e4ecb1ec1cd2e6e7f7076b0ff94f
Jeff Hunter <jeff_hunter@sv.comcast.com>
2014-11-06 10:30:10 -0800

    Fix memory tag errors and update tests to reflect inclusion of encoding information in produced XML

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var libxmljs = require('libxmljs')
   , xqjs = require('./build/Release/xqjs')
+  , util = require('util')
 ;
 
 // Add JavaScript-based utility functions
@@ -17,4 +18,37 @@ xqjs.xQ.prototype.map = function(iterator, context) {
   return results;
 }
 
-module.exports = xqjs.xQ;
+/**
+ * Wrap the native constructor with routines that normalize how it is
+ * called.
+ */
+function xQ() {
+  
+  var nodes = [];
+  
+  function addArgs(args) {
+    var len = args.length;
+    for (var i = 0; i < len; ++i) {
+      var a = args[i];
+      var t = typeof a;
+      
+      if ("array" === t)
+        addArgs(a);
+      else if ("object" === t)
+        nodes.push(a);
+      else
+        nodes.push(libxmljs.parseXml(String(a)));
+    }
+  }
+  
+  addArgs(arguments);
+  
+  if (this instanceof xQ)
+    return xqjs.xQ.call(this, nodes);
+  else
+    return new xqjs.xQ(nodes);
+}
+
+util.inherits(xQ, xqjs.xQ);
+
+module.exports = xQ;
